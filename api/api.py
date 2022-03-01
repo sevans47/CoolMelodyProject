b570bb47fd28fdb3e9367000577acaf2c7e0a123
Mizuki Nakano <mizukin1187@gmail.com>
2022-03-01 10:10:13 +0900

    added api codes

---
+++ api/api.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_orifins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+@app.get('/')
+def index():
+    return {";)" : "hi handsome"}
